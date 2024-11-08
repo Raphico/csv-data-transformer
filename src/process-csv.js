89b9eb14a27bfc35e2b18a13400521bd28c6fa79
Raphico <raphicogit@gmail.com>
2024-11-08 10:50:46 +0100

    feat: implement remove column

--- src/process-csv.js
+++ src/process-csv.js
@@ -14,23 +14,45 @@ export async function processCSV(argv) {
         input: inputStream,
     });
 
-    let isHeader = true;
+    let isHeader = true,
+        isFilterValid,
+        columnToRemoveIndex,
+        noTransformation = false;
     let headers;
 
     for await (const line of rl) {
         if (isHeader) {
             headers = line.split(",");
-            outStream.write(`${line}\n`);
             isHeader = false;
-            continue;
-        }
 
-        if (argv.filter && !isEmpty(argv.filter)) {
-            if (!isValidFilterValue(argv.filter, headers)) {
-                printError("invalid filter value or field not found");
+            [isFilterValid, columnToRemoveIndex] = validateOptions(
+                argv,
+                headers
+            );
+
+            if (isFilterValid === false) {
+                printError("invalid filter value or column not found");
                 return;
             }
 
+            if (columnToRemoveIndex === -1) {
+                printError("column not found");
+                return;
+            }
+
+            if (columnToRemoveIndex > -1)
+                headers.splice(columnToRemoveIndex, 1);
+
+            noTransformation =
+                isFilterValid === undefined &&
+                columnToRemoveIndex === undefined;
+
+            outStream.write(`${headers.join(",")}\n`);
+
+            continue;
+        }
+
+        if (isFilterValid) {
             const shouldIncludeLine = applyFilter({
                 filterValue: argv.filter,
                 headers,
@@ -38,7 +60,42 @@ export async function processCSV(argv) {
                 delimiter: argv.delimiter,
             });
 
-            shouldIncludeLine && outStream.write(`${line}\n`);
+            if (!shouldIncludeLine) continue;
+        }
+
+        if (columnToRemoveIndex > -1) {
+            const newLine = line
+                .split(",")
+                .filter((_, index) => index != columnToRemoveIndex)
+                .join(",");
+            outStream.write(`${newLine}\n`);
+        }
+
+        if (noTransformation) {
+            outStream.write(`${line}\n`);
         }
     }
+
+    if (noTransformation) {
+        console.log(
+            "Output file created with all original content as no transformation options were specified."
+        );
+        console.log("Try 'bfr --help' for more information.");
+    }
+}
+
+export function validateOptions(argv, headers) {
+    let isFilterValid, columnToRemoveIndex;
+
+    if (argv.filter && !isEmpty(argv.filter)) {
+        isFilterValid = isValidFilterValue(argv.filter, headers);
+    }
+
+    if (argv["remove-column"] && !isEmpty(argv["remove-column"])) {
+        columnToRemoveIndex = headers.findIndex(
+            (header) => header === argv["remove-column"]
+        );
+    }
+
+    return [isFilterValid, columnToRemoveIndex];
 }
